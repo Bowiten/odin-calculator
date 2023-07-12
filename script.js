96d1efa778618c8c0c1215fee9db8bedfd45c0db
Bowiten <71778489+Bowiten@users.noreply.github.com>
2023-07-12 14:13:22 -0700

    Add error if dividing by 0

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    return num1 / num2;
+    return (num2 != 0 ? num1 / num2 : 'Error');
 }
 
 function operate(num1, operator, num2) {
