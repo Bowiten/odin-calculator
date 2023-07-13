45efe90d06cdf5ef8e6f8481ed726304bef76f1e
Bowiten <71778489+Bowiten@users.noreply.github.com>
2023-07-13 16:55:43 -0700

    Implement equals button

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const displayResult = document.querySelector('.result');
 const digitButtons = document.querySelectorAll('.digit');
 const operationButtons = document.querySelectorAll('.operation');
+const evalButton = document.querySelector('.eval')
 
 let num1 = 0;
 let operator = '';
@@ -59,11 +60,22 @@ function execOperation(operation) {
         num2 = parseFloat(displayResult.textContent);
         result = operate();
         updateDisplay(result);
-        operator = operation;
         clearOnNextInput = true;
         num2 = 0;
         num1 = result;
+        operator = operation;
     }
+}
+
+function evaluate() {
+    if (operator == '')
+        return;
+    num2 = parseFloat(displayResult.textContent);
+    haha = operate();
+    updateDisplay(haha);
+    clearOnNextInput = true;
+    num2 = 0;
+    num1 = haha;
 }
 
 digitButtons.forEach(button => button.addEventListener('click', () => {
@@ -73,3 +85,5 @@ digitButtons.forEach(button => button.addEventListener('click', () => {
 operationButtons.forEach(button => button.addEventListener('click', () => {
     execOperation(button.textContent);
 }))
+
+evalButton.addEventListener('click', () => evaluate());
