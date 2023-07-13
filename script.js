a808801a9a7f82c45569030e34ee4488cdd5efa6
Bowiten <71778489+Bowiten@users.noreply.github.com>
2023-07-13 10:42:47 -0700

    Add code for operations

---
+++ script.js
@@ -0,0 +1,32 @@
+let num1 = 0;
+let operator = '';
+let num2 = 0;
+
+const add = function() {
+    return num1 + num2;
+}
+
+const subtract = function() {
+    return num1 - num2;
+}
+
+const multiply = function() {
+    return num1 * num2;
+}
+
+const divide = function() {
+    return (num2 != 0 ? num1 / num2 : `You can't divide by zero!`);
+}
+
+function operate(num1, operator, num2) {
+    switch (operator) {
+        case '+':
+            return add;
+        case '-':
+            return subtract;
+        case '*':
+            return multiply;
+        case '/':
+            return divide;
+    }
+}
